e12ec731c2cf4e5e871a6e40e51f319f1be4c422
Ryan6338 <ryan_marple@hotmail.com>
2013-02-15 22:52:14 +1000

    Dev build v0.1 working!

--- src/me/Ryan6338/ColouredPlayerList/ColouredPlayerList.java
+++ src/me/Ryan6338/ColouredPlayerList/ColouredPlayerList.java
@@ -8,21 +8,37 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ColouredPlayerList extends JavaPlugin {
 	public final Logger logger = Logger.getLogger("Minecraft");
-	public ColouredPlayerList plugin;
 	public PlayerListener listener;
+	private Permissions permissions = new Permissions(this);
 
+	//Performs on Disable
+	
 	@Override
 	public void onDisable() {
 		PluginDescriptionFile pdfFile = this.getDescription();
-		this.logger.info(pdfFile.getName() + " has been disabled!");
+		logger.info(pdfFile.getName() + " has been disabled!");
 	}
 
+	//Performs on Enable if the version is correct
+	
 	@Override
 	public void onEnable() {
-		PluginDescriptionFile pdfFile = this.getDescription();
-		this.logger
-				.info(pdfFile.getName() + " Version " + " has been enabled!");
 		PluginManager pm = this.getServer().getPluginManager();
-		pm.registerEvents(new PlayerListener(), this);
+		
+		//Registers events in the PlayerListener class
+		
+		pm.registerEvents(new PlayerListener(this), this);
+	}
+	
+	//Allows Permissions to have an instance of ColouredPlayerList
+	
+	public Permissions getPermissions() {
+		return permissions;
+	}
+	
+	//Logs in the console messages from the plugin
+	
+	public void Log(String text) {
+		logger.info("[ColouredPlayerList] " + text);
 	}
 }
--- src/me/Ryan6338/ColouredPlayerList/PlayerListener.java
+++ src/me/Ryan6338/ColouredPlayerList/PlayerListener.java
@@ -1,15 +1,41 @@
 package me.Ryan6338.ColouredPlayerList;
 
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PlayerListener implements Listener {
 	public PlayerListener listener;
-	PermsLookup perms = new PermsLookup(null);
+	private ColouredPlayerList plugin;
+	
+	public PlayerListener(ColouredPlayerList instance) {
+		plugin = instance;
+	}
+	
+	//Performs when a player joins the game
 	
 	@EventHandler
 	public void onPlayerJoinEvent(PlayerJoinEvent event) {
-		perms.colour(event.getPlayer());
+		final Player p = event.getPlayer();
+		
+		final ChatColor c = plugin.getPermissions().getColour(p);
+
+		//Delays the task to check for the player display name to give display names time to load
+		
+		plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+			public void run() {
+				String pname = p.getDisplayName();
+				
+				//Checks to see if the nickname is too long
+				
+				if (p.getDisplayName().length() > 14) {
+					pname = p.getDisplayName().substring(0, 14);
+				}
+				plugin.Log("Player name: " + pname);
+				p.setPlayerListName(c + pname);
+			}
+		}, 2L);
 	}
 }
